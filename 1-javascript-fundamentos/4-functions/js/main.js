4b3545e97a76bab47a829aab6cea6585c3f4f413
JoseWilsonPaulaGrilo <32718374+JoseWilsonPaulaGrilo@users.noreply.github.com>
2019-07-10 20:53:54 -0300

    Adcionados main.js: functions e condicionais

--- 1-javascript-fundamentos/4-functions/js/main.js
+++ 1-javascript-fundamentos/4-functions/js/main.js
@@ -10,10 +10,31 @@ function minhaFuncao(name) {
   return `Olá, ${name}`
 }
 
+function soma(x,y){
+  return (x + y);
+}
+function subtracao(x,y){
+  return x-y;
+}
+function multiplicacao(x,y){
+  return x*y;
+}
+function divisao(x,y){
+  return (x/y);
+}
+function fullName(nome,sobrenome){
+  return nome + sobrenome;
+}
+
 // const minhaFuncao = function (name) {
 //   return `Olá, ${name}`
 // }
 
 // const minhaFuncao = name => `Olá, ${name}`
 
-console.log(minhaFuncao())
+console.log(minhaFuncao());
+console.log(soma());
+console.log(subtracao());
+console.log(multiplicacao());
+console.log(divisao());
+console.log(fullName());
--- 1-javascript-fundamentos/6-condicionais/js/main.js
+++ 1-javascript-fundamentos/6-condicionais/js/main.js
@@ -2,6 +2,28 @@ const a = 10
 const b = 5
 const c = '5'
 
+console.log('igual a ', b == c);
+console.log('identico a ', b === c);
+console.log('diferente ', b!= c);
+console.log('realmente é diferente ', b !== c);
+console.log('maior ', a > b);
+console.log('maior ou igual ', a >= b);
+console.log('menor ', a < b);
+console.log('maior ou igual', a <= b);
+
+if(4 > 2){
+  console.log('é');
+}else{
+  console.log('não é');
+}
+
+const maior = (4 > 2) ? 'é' : 'não é';
+console.log(maior);
+
+console.log('maior ->', a > b && b == c);
+console.log('maior ->', a > b || b == c);
+console.log('maior ->', !(a > b) && b == c);
+
 /*
   Operadores de comparação
   - igual a
@@ -16,8 +38,8 @@ const c = '5'
 
 /*
  Operadores Lógicos
- - e
- - ou
+ - e &&
+ - ou ||
  - negação
 */
 
